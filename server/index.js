fcc7d600616e8e4f2a4fcb50be9663729c197514
Juan Fernando Toro <juanfertoro192@gmail.com>
2021-12-16 18:39:40 +0100

    API initialize (models, controllers, routes) + redis connection

--- server/index.js
+++ server/index.js
@@ -3,7 +3,26 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config()
 
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("../config");
+const { 
+    MONGO_USER, 
+    MONGO_PASSWORD, 
+    MONGO_IP, 
+    MONGO_PORT, 
+    SESSION_SECRET,
+    REDIS_URL,
+    REDIS_PORT
+} = require("../config");
+
+const redis = require('redis')
+const session = require('express-session')
+let RedisStore = require('connect-redis')(session)
+let redisClient = redis.createClient({
+    host: REDIS_URL,
+    port: REDIS_PORT
+})
+
+const postRouter = require("./routes/postRoutes")
+const userRouter = require("./routes/userRoutes")
 
 const app = express()
 
@@ -29,8 +48,9 @@ const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_
 const connectWithRetry = () =>{
     mongoose
     .connect(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        // No longer required mongo 6.0
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
         // useFindAndModify: false
     })
     .then(() => console.log("successfully connected to DB"))
@@ -42,11 +62,30 @@ const connectWithRetry = () =>{
 
 connectWithRetry();
 
+app.use(express.json());
+
+app.use(
+    session({
+      store: new RedisStore({ client: redisClient }),
+      secret: SESSION_SECRET,
+      saveUninitialized: false,
+      resave: false,
+    //   TODO: No cookies on response
+      cookie: { 
+          secure: false,
+          resave: false,
+          httpOnly: true,
+          maxAge: 30 * 1000,
+          saveUninitialized: false, 
+        },
+    })
+  )
+
 console.log(__dirname);
 app.use(express.static(__dirname +'/public/'))
-
 app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index.html'))
-
+app.use("/api/v1/posts",postRouter)
+app.use("/api/v1/users",userRouter)
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
