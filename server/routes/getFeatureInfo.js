049e79fa1333b77fed2f23ceef90b8e84faf7a89
juanfernando.toro <juanfernando.toro@polimi.it>
2022-04-28 16:38:35 +0200

    Added TS plot function

--- server/routes/getFeatureInfo.js
+++ server/routes/getFeatureInfo.js
@@ -10,7 +10,7 @@ const API_BASE_URL = "https://www.geonode.eo.simile.polimi.it/geoserver/ows"
 
 
 router.get('/', async (req,res) => {
-    console.log(url.parse(req.url,true).query);
+    // console.log(url.parse(req.url,true).query);
     try {
         const params = new URLSearchParams({
             // ['key'] : API_KEY_VALUE
@@ -22,7 +22,7 @@ router.get('/', async (req,res) => {
 
         res.status(200).json({data})
     } catch {
-        res.status(500).json({success:false})
+        // res.status(500).json({success:false})
     }
 
 
